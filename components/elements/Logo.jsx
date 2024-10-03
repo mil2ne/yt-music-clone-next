ee35dd4a9a28f4a438cb9c3739f7915eef6b0979
celee <show3070@gmail.com>
2024-10-03 20:29:55 +0900

    #9.Sidebar-Logo

--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -1,7 +1,31 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import IconButton from "./IconButton";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  const { push } = useRouter();
+  const onClickLogo = () => {
+    push("/");
+  };
+  const onClickMenu = () => {};
+
+  return (
+    <div>
+      <section className=" flex flex-row gap-3 items-center">
+        <IconButton
+          onClickIcon={onClickMenu}
+          icon={<RxHamburgerMenu size={24} />}
+        />
+        <div className=" cursor-pointer" onClick={onClickLogo}>
+          <Image alt="logo" width={100} height={30} src={"/main-logo.svg"} />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Logo;
