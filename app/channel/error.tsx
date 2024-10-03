66a5bde88b12fa8f72d6dad0025ff448e3e8d4b4
celee <show3070@gmail.com>
2024-10-03 10:05:53 +0900

    #6. Loading, Error

---
+++ app/channel/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ErrorMessage from "@/components/ErrorMessage";
+import React from "react";
+import { GridLoader } from "react-spinners";
+
+const error = () => {
+  return <ErrorMessage />;
+};
+
+export default error;
